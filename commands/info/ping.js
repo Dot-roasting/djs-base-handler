6517be9394299bb9d453464cdf68b903fdd667ce
EPD <75964362+Dot-roasting@users.noreply.github.com>
2022-03-22 00:13:55 +0530

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,7 +3,11 @@ const { Message, Client } = require("discord.js");
 module.exports = {
     name: "ping",
     aliases: ['p'],
-    memberPermissions: ['ADMINISTRATOR']
+    memberPermissions: ['ADMINISTRATOR'], //Member Permission Check
+    botPerms: ['ADMINISTRATOR'], //Bot Permission check in server
+    botChannelPerms: ['EMBED_LINKS'], // Bot permission check in Channel
+    owner: true, //Owner Only check
+    serverOwner: true, //Server Owner check
     /**
      *
      * @param {Client} client
