b2e689f70385bad263d0273a487f0fcc78910c25
Zach J Murphy <78761892+zachjmurphy@users.noreply.github.com>
2022-03-22 15:48:51 -0400

    Fixed Styling Issue

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,11 +3,11 @@ const { Message, Client } = require("discord.js");
 module.exports = {
     name: "ping",
     aliases: ['p'],
-    memberPermissions: ['SEND_MESSAGES'], //Member Permission Check
-    botPerms: ['SEND_MESSAGES'], //Bot Permission check in server
+    memberPermissions: ['SEND_MESSAGES'], // Member Permission Check
+    botPerms: ['SEND_MESSAGES'], // Bot Permission check in server
     botChannelPerms: ['SEND_MESSAGES'], // Bot permission check in Channel
-    owner: true, //Owner Only check
-    serverOwner: true, //Server Owner check
+    owner: true, // Owner Only check
+    serverOwner: true, // Server Owner check
 
     /**
      *
